5b8e9c5480b634fce035f0d4301189bfbff96eeb
aarondill <aaronsacks2006@gmail.com>
2023-01-30 08:33:30 -0600

    improve testing framework

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,18 +1,4 @@
 /* eslint-disable no-unused-vars */
-/** Import constant data from `./data.example.ts` */
-import { exportedForTesting } from "../src/index";
-
-const {
-	betweenTime,
-	findBlueOrange,
-	findPeriod,
-	getDatesFromStrings,
-	isHoliday,
-	isWeekend,
-	openCanvas,
-	toMilliseconds,
-	main,
-} = exportedForTesting;
 
 import { DATA } from "./data.example";
 /** Force consistent data for main test */
@@ -35,26 +21,57 @@ windowSpy.mockImplementation(() => {
 	// ^ typescript, pls shush
 });
 
-describe("Test betweenTime", () => {
+//!! This *does not* run the code in the import, only imports the types of the export
+// This is required to keep type-safety while later dynamically importing.
+type ExportedForTesting = typeof import("../src/index")["exportedForTesting"];
+let betweenTime: ExportedForTesting["betweenTime"],
+	findBlueOrange: ExportedForTesting["findBlueOrange"],
+	findPeriod: ExportedForTesting["findPeriod"],
+	getDatesFromStrings: ExportedForTesting["getDatesFromStrings"],
+	isHoliday: ExportedForTesting["isHoliday"],
+	isWeekend: ExportedForTesting["isWeekend"],
+	openCanvas: ExportedForTesting["openCanvas"],
+	toMilliseconds: ExportedForTesting["toMilliseconds"],
+	main: ExportedForTesting["main"];
+
+// Ensure presence of import before anything runs
+beforeAll(async () => {
+	const { exportedForTesting } = await import("../src/index");
+	({
+		betweenTime,
+		findBlueOrange,
+		findPeriod,
+		getDatesFromStrings,
+		isHoliday,
+		isWeekend,
+		openCanvas,
+		toMilliseconds,
+		main,
+	} = exportedForTesting);
+});
+
+describe("betweenTime", () => {
+	let date: Date;
+	beforeEach(() => {
+		date = new Date();
+		date.setHours(1); // 1 AM
+		date.setMinutes(0); // 1:00 AM
+		date.setSeconds(0); // 1:00:00 AM
+	});
+
 	it("returns a boolean", () => {
 		expect(typeof betweenTime(new Date(), 0, 1)).toBe("boolean");
 	});
-	const date = new Date();
-	date.setHours(1); // 1 AM
-	date.setMinutes(0); // 1:00 AM
-	date.setSeconds(0); // 1:00:00 AM
 
 	const twoAMInMilliseconds = 72e5;
 	const midnightInMilliseconds = 0;
 	it("Returns true", () => {
+		// date: 1 am
 		expect(betweenTime(date, midnightInMilliseconds, twoAMInMilliseconds)) //
 			.toBe(true);
 	});
 	it("Returns false", () => {
-		date.setHours(3); // 3 AM
-		date.setMinutes(0); // 3:00 AM
-		date.setSeconds(0); // 3:00:00 AM
-
+		date.setHours(3); // 3:00:00 AM
 		expect(betweenTime(date, midnightInMilliseconds, twoAMInMilliseconds)) //
 			.toBe(false);
 	});
